f5bcfeb6a7b0808b3b4c0c500ddff5ea6c901d91
Geshina <geshovskileo@gmail.com>
2024-04-16 12:17:45 +0200

    idk

---
+++ src/main.js
@@ -0,0 +1,6 @@
+import './osseus/CSS/_config.css'
+import './osseus/CSS/color.css'
+import './osseus/CSS/reset.css'
+import './osseus/CSS/structure.css'
+import './osseus/CSS/typography.css'
+import './osseus/CSS/utility.css'
