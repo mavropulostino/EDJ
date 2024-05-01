b84528a3aceb46e34ed9a6bc9a9dab533721a3e4
Geshina <geshovskileo@gmail.com>
2024-05-01 14:22:42 +0200

    sprint chaos

--- src/main.js
+++ src/main.js
@@ -5,3 +5,8 @@ import './osseus/CSS/reset.css'
 import './osseus/CSS/structure.css'
 import './osseus/CSS/typography.css'
 import './osseus/CSS/utility.css'
+
+import { globalEventHandler } from './osseus/JS/handlers'
+import { singleStateToggle } from './osseus/JS/state'
+
+globalEventHandler('click', '._faq_origin', singleStateToggle)
