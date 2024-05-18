1f0d0d9da7ae4e88969598b848ae3af3fa41c2a2
Geshina <geshovskileo@gmail.com>
2024-05-18 14:23:59 +0200

    pre-parent

--- src/shop.js
+++ src/shop.js
@@ -1,3 +1,9 @@
+// define parent which contains reactive pattern and DOM manip methods
+// product and cart extend from parent
+// product contains productAmount and productTotal
+// cart contains cartAmount and cartTotal + cPayPost
+// optimize by making universal amount and total methods on parent
+
 class Product {
   constructor(name, price) {
     this.name = name
@@ -21,22 +27,24 @@ class Cart {
     if (product) product.amount = Math.max(0, --product.amount)
     this.updateDOM()
   }
-
-  getTotal = () => {
-    return Object.values(this.products).reduce(
-      (total, product) =>
-        total +
-        (product.amount * product.price * (100 - this.salePercent)) / 100,
+  getTotal = () =>
+    this.products.reduce(
+      (total, { amount, price }) =>
+        total + amount * price * (1 - this.salePercent / 100),
       0
     )
-  }
+  // .toFixed(2)
+
   updateDOM = () => {
-    const updates = [['.prod-total', this.total.toFixed(2)], []]
+    const updates = [
+      ['.prod-total', this.total],
+      ['.prod-amount', this.amount],
+    ]
 
     updates.forEach(([selector, value]) => {
       const element = document.querySelector(selector)
       if (element) element.textContent = value
-      if (!element) alert('element not found')
+      if (!element) alert(`${selector} not found`)
     })
   }
 
