998bce5106d82fd843633da98d3cfe3fcb1f5506
YeetusDilitus <theyeetusdilitus69@gmail.com>
2024-06-09 18:29:36 +0200

    Minor HTML, CSS change

--- src/main.js
+++ src/main.js
@@ -10,29 +10,13 @@ import { globalEventHandler } from './OSS-temp/JS/handlers'
 import { singleStateToggle } from './OSS-temp/JS/state'
 import { Cart, Product } from './shop'
 
-let two_day_signature = new Product(
-  'Two_Day_Signature',
-  'Дводневен signature',
-  2000
-)
-let three_day_signature = new Product(
-  'Three_Day_Signature',
-  'Тридневен signature',
-  2850
-)
-let four_day_signature = new Product(
-  'Four_Day_Signature',
-  'Четиридневен signature',
-  3600
-)
+let two_day_signature = new Product('Two_Day_Signature','Дводневен signature',2000)
+let three_day_signature = new Product('Three_Day_Signature','Тридневен signature',2850)
+let four_day_signature = new Product('Four_Day_Signature','Четиридневен signature',3600)
 ///
 let two_day_immuno = new Product('Two_Day_Immuno', 'Дводневен immuno', 1900)
 let three_day_immuno = new Product('Three_Day_Immuno', 'Тридневен immuno', 2700)
-let four_day_immuno = new Product(
-  'Four_Day_Immuno',
-  'Четиридневен immuno',
-  3400
-)
+let four_day_immuno = new Product('Four_Day_Immuno','Четиридневен immuno',3400)
 
 let cart = new Cart(
   two_day_signature,
